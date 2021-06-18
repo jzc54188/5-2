d43eadabba4d785fda1c03b0484f9927cce1c788
jzc54188 <jzc@qq.com>
2021-06-18 08:00:13 +0800

    second commit 6-18

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div>
-      React
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        react 5-2
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
